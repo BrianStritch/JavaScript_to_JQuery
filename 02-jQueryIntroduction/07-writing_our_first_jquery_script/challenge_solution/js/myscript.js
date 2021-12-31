f9e454c0b6bf58fd670bc85de42f81458860353f
BrianStritch <brians12ca18@gmail.com>
2021-12-31 00:36:06 +0000

    add content to 07-writing our first j query script

--- 02-jQueryIntroduction/07-writing_our_first_jquery_script/challenge_solution/js/myscript.js
+++ 02-jQueryIntroduction/07-writing_our_first_jquery_script/challenge_solution/js/myscript.js
@@ -1,3 +1,17 @@
+// $(document).ready(function() {
+//     $("tr:odd").addClass("odd");
+//     $("tr:even").addClass("even");
+// });
+
+// $(document).ready(function() {    // only executes after the document is fully loaded
+//     $('h2').css('text-decoration', 'underline'); // underslines all h2 elements
+//     $('h2').addClass('underline'); // adds the class of underline to all h2 elements
+//     $('h2').removeClass('underline'); // removes the class of underline to all h2 elements
+//     $('ul').css('border', 'solid 2px #ccc');  // creates a border around any ul elements
+//     $('ul').addClass('border'); // adds the class of border to all ul elements
+//     $('ul').removeClass('border'); // removes the class of border to all ul elements
+// });
+
 $(document).ready(function() {
     $("tr:odd").addClass("odd");
     $("tr:even").addClass("even");
